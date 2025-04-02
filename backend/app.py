109360c602a6c9ae4d4502d881294c1b767b3759
liupeng413 <liupbroccoli@gmail.com>
2025-04-03 07:07:39 +0800

    修改配置以支持外部访问

--- backend/app.py
+++ backend/app.py
@@ -400,4 +400,4 @@ def update_employee_number(id):
     return redirect(url_for('list_teachers'))
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True)
